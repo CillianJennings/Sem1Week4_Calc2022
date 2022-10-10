1f028a7ef2961bacfdddb3be95c3d1e7074a25a1
Gavin Walsh <G00402296@atu.ie>
2022-10-10 10:38:56 +0100

    Added Add function

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,5 +1,18 @@
 package ie.atu.week4;
 
+
 public class Calc {
-    //fojafgj
+    public static void main(String[] args) {
+
+    }
+
+
+    public static int add(int Num1, int Num2)
+    {
+        int addition = Num1 + Num2;
+        return addition;
+
+    }
 }
+
+
