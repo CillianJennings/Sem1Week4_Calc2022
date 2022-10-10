8dcda698d36b8536dc34714f211ae368b4c780ae
Cillian Jennings <G00404411@atu.ie>
2022-10-10 10:19:47 +0100

    Added multiply

--- src/main/java/ie/atu/week4/Calc.java
+++ src/main/java/ie/atu/week4/Calc.java
@@ -1,5 +1,17 @@
 package ie.atu.week4;
 
+import java.util.Scanner;
+
 public class Calc {
-    //fojafgj
+    public static void main(String[] args) {
+
+
+    }
+
+    public static int multiply(int Num1, int Num2){
+
+        int multiply = Num1 * Num2;
+        return multiply;
+    }
+
 }
